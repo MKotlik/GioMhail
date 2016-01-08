8cf7a8fa6ea9a24742daf5fe2e386b87bb913c77
gTopa <gtopa@stuy.edu>
2016-01-08 18:46:57 -0500

    Finished RCPT and DATA, compiles

--- SMTP.java
+++ SMTP.java
@@ -16,7 +16,7 @@ public class SMTP{
 	    System.out.println("unknown host");
 	}
     }
-    public boolean send(String message, String to, String from){
+    public boolean send(String sub, String message, String to, String from){
 	try{
 	    Socket smtpPipe=new Socket(mailHost,SMTP_PORT);
 	    if(smtpPipe==null){
@@ -26,13 +26,33 @@ public class SMTP{
 	    OutputStream os=smtpPipe.getOutputStream();
 	    in=new BufferedReader(new InputStreamReader(is));
 	    out=new PrintWriter(new OutputStreamWriter(os),true);
+	    System.out.print("HELO "+localHost.getHostName());
 	    out.print("HELO "+localHost.getHostName());
 	    String response=in.readLine();
 	    System.out.println("response: "+response);
-	    if(not response.substring(0,1).equals("2")){
+	    if(! response.substring(0,1).equals("2")){
 		return false;
 	    }
-	    out.print("MAIL <"+to+">");
+	    System.out.print("MAIL From:<"+from+">");
+	    out.println("MAIL From:<"+from+">");
+	    response=in.readLine();
+	    System.out.println("response: "+response);
+	    if(! response.substring(0,1).equals("2")){
+		return false;
+	    }
+	    System.out.println("RCPT To:<"+to+">");
+	    out.println("RCPT To:<"+to+">");
+	    response=in.readLine();
+	    System.out.println("response: "+response);
+	    System.out.println("DATA");
+	    out.println("DATA");
+	    response=in.readLine();
+	    if(! response.substring(0,3).equals("354")){
+		return false;
+	    }
+	    out.println("Subject: "+sub);
+	    out.println(message);
+	    out.println(".");
 	}catch(IOException e){
 	    System.out.println("IOException");
 	}
