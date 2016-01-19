830f4f1683997f6160284786c724eaf590c75031
gTopa <gtopa@stuy.edu>
2016-01-18 19:46:08 -0500

    finished sendMessage

--- SMTPSession.java
+++ SMTPSession.java
@@ -7,11 +7,40 @@
 import java.util.*;
 
 public class SMTPSession extends Session {
-
+    private HeaderStore header;
+    
     //SMTPSession constructor
     public SMTPSession(String host, int port) {
         super(host, port, "SMTP");
     }
-
+    
+    public boolean sendMail(Message newMsg){
+	header=newMsg.getHeaderStore();
+	writeServer("MAIL From: " + header.getFrom());
+	String serverInput=read(false).get(0);
+	if(checkResponseCode(serverInput, "250")){
+	    writeServer("RCPT To: " + header.getTo());
+	    serverInput=read(false).get(0);
+	    if(checkResponseCode(serverInput, "250")){
+		writeServer("DATA");
+		serverInput=read(false).get(0);
+		if(checkResponseCode(serverInput, "354")){
+		    writeServer(newMsg.getMessageBody());
+		    serverInput=read(true).get(0);
+		    if(checkResponseCode(serverInput, "250")){
+			return true;
+		    }else{
+			return false;
+		    }
+		}else{
+		    return false;
+		}
+	    }else{
+		return false;
+	    }
+	}else{
+	    return false;
+	}
+    }
 
 }
