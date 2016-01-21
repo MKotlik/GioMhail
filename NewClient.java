868ce15005c9707f69fb75c88190cf159a29d0d2
Mikhail Kotlik <mkotlik@stuy.edu>
2016-01-21 11:26:38 -0500

    Added plan to remake Client, began remake with NewClient.java

---
+++ NewClient.java
@@ -0,0 +1,18 @@
+/* GioMhail by Coolgle
+   - NewClient
+   - Copyright (c) 2016, Giovanni Topa and Mikhail Kotlik, All Rights Reserved.
+   - APCS Term 1 Final Project, Stuyvesant High School
+*/
+
+/* This is a remake of the original Client!
+   It is to be renamed after completion.
+ */
+
+/* TODO
+ - Copy imports
+ - Define instance vars
+ - Default constructor
+ - Main function
+ - Create function for each mode
+ - ... See client_remake_plan.txt in Resources for more info
+ */
