0ce5411284b06ea463414de70507291ca0529f6b
gTopa <gtopa@stuy.edu>
2016-01-22 11:51:32 -0500

    created Storage class, and saveMessage

---
+++ Storage.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class Storage{
+    private PrintWriter out;
+
+    public String saveMessage(Message newMsg){
+	try{
+	    out=new PrintWriter("file.txt");
+	    writeHeader(newMsg, newMsg.getHeaderStore());
+	    writeMessage(newMsg);
+	    out.close();
+	    return "SUCCESS";
+	}catch(FileNotFoundException e){
+	    return "ERROR";
+	}
+    }
+
+    private void writeHeader(Message newMsg, HeaderStore msgHeaders){
+        String[] headerKeys = msgHeaders.getKeyArray();
+        for (int i = 0; i < headerKeys.length; i++) {
+            ArrayList<String> headerValueList = msgHeaders.getHeaderValue(headerKeys[i]);
+            for (int j = 0; j < headerValueList.size(); j++) {
+                String line = headerKeys[i] + ": " + headerValueList.get(j);
+                out.println(line);
+            }
+        }
+    }
+
+    private void writeMessage(Message newMsg){
+	out.print(newMsg.getMessageBody());
+    }
+
+    private int createFileName(HeaderStore msgHeaders){
+	String unHashed = "";
+	String To = msgHeaders.getTo();
+	String From = msgHeaders.getFrom();
+	String Date = msgHeaders.getDate();
+	unHashed = To+":"+From+":"+Date;
+	return unHashed.hashCode();
+    }
+
+}
